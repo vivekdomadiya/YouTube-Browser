f01bb6bb4cdd0791eee391801afc544ff1118ff8
VivekDomadia <domadia.vivek.2@ldce.ac.in>
2020-07-19 15:48:30 +0530

    Updates

---
+++ precache-manifest.626f5dfcb86ce3d40758a1da75211f06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f5dfdcf5948bd102572eaadbd320cee",
+    "url": "/YouTube-Browser/index.html"
+  },
+  {
+    "revision": "45107108cbdd305f6b22",
+    "url": "/YouTube-Browser/static/css/main.8ad48a67.chunk.css"
+  },
+  {
+    "revision": "02e0aaa203a358755033",
+    "url": "/YouTube-Browser/static/js/2.51bb68e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/YouTube-Browser/static/js/2.51bb68e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45107108cbdd305f6b22",
+    "url": "/YouTube-Browser/static/js/main.f04bdaa4.chunk.js"
+  },
+  {
+    "revision": "8e6c04e4d59a75a0a23a",
+    "url": "/YouTube-Browser/static/js/runtime-main.b64987e2.js"
+  }
+]);
